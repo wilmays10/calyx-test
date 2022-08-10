de517ff591a80af48f3975310e3e52021f2f9c57
calaveralma <wilmays10@gmail.com>
2022-08-10 17:59:14 -0300

    fix: correcciones en nombres y tipos de la tabla principal

--- create_table_main.sql
+++ create_table_main.sql
@@ -1,15 +1,15 @@
 CREATE TABLE centro_cultural (
     id serial PRIMARY KEY,
-    cod_localidad int,
-    id_provincia int,
-    id_departamento int,
-    categoria varchar (50) check (categoria in ('museo', 'biblioteca', 'cine')),
-    provincia varchar (30),
-    localidad varchar (50),
-    nombre varchar (50),
-    domicilio varchar (50),
-    codigo_postal varchar (30),
-    numero_de_telefono int,
-    mail varchar (50),
-    web varchar (50)
+    cod_loc int,
+    idprovincia int,
+    iddepartamento int,
+    categoria varchar (50) check (categoria in ('museos', 'bibliotecas', 'cines')),
+    provincia varchar (70),
+    localidad varchar (100),
+    nombre varchar (120),
+    direccion varchar (150),
+    cp varchar (30),
+    telefono varchar(30),
+    mail varchar (80),
+    web varchar (200)
 );
